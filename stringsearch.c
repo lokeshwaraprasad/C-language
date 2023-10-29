f34c2460d9d3edf2d29c75544aba34c0c5417d80
Lokeshwaraprasad <venkatesanlokeshwaraprasad@gmail.com>
2023-10-29 14:05:17 +0530

    to search a character in a  string

---
+++ stringsearch.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+    char s[1000],c;  
+    int i;
+ 
+    printf("Enter  the string : ");
+    gets(s);
+    printf("Enter character to be searched: ");
+    c=getchar();
+    
+    for(i=0;s[i];i++)  
+    {
+    	if(s[i]==c)
+    	{
+		    printf("character '%c' found at location: %d\n ",c,i);
+ 
+		}
+ 	}
+     
+ 
+ 	 
+     
+    return 0;
+}
