6e54c3843a6c4178634f38ea48816851ecf863c0
Lokeshwaraprasad <venkatesanlokeshwaraprasad@gmail.com>
2023-11-11 15:56:24 +0530

    print alphabets using single array of strring.C

---
+++ alphabetss.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main()
+{
+int y;
+int u=26;
+char all[150];
+
+ for(char z='a';z<='z';z++)
+ {
+    all[y] =z;
+    y++;
+ }
+ for(char h='A';h<='Z';h++)
+ {
+     all[u]=h;
+     u++;
+ }
+ printf("%s\n",all);
+
+}
