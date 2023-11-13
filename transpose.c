e06bf5b9be53800c1928beb7f1f977a56ebe369e
Lokeshwaraprasad <venkatesanlokeshwaraprasad@gmail.com>
2023-11-13 10:54:03 +0530

    transpose of a matrix in c

---
+++ transpose.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+int main()
+{
+   int m,n;
+   printf("ENTER NUMBER OF ROWS:  ");
+   scanf("%d",&m);
+   printf("\n ENTER THE NUMBER OF COLUMNS: ");
+   scanf("%d",&n);
+   int A[10][10];
+  for (int i =0;i<m;i++)
+  {
+      for(int j=0;j<n;j++)
+      {
+          printf("ENTER THE ELEMENT IN %d %d: ",i,j);
+          scanf(" %d",&A[i][j]);
+      }
+  }
+  int B[10][10];
+  for (int i=0;i<m;++i)
+  {
+      for(int j=0;j<n;j++)
+      {
+          B[i][j]=A[i][j];
+          
+      }
+  }
+  printf("\nTranspose of the matrix:\n");
+  for (int i = 0; i < n; ++i)
+  for (int j = 0; j < m; ++j) {
+    printf("%d  ", B[i][j]);
+    if (j == m- 1)
+    printf("\n");
+  }
+  
+ return 0;
+}
